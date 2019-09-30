8ca6fb2107e389598dfdd089b3b5262a274ef232
Brendon Chiang <brendon.chiang@gmail.com>
2019-09-30 15:27:28 -0700

    Open/Close

--- CSdict.java
+++ CSdict.java
@@ -1,7 +1,7 @@
 
 // You can use this file as a starting point for your dictionary client
 // The file contains the code for command line parsing and it also
-// illustrates how to read and partially parse the input typed by the user. 
+// illustrates how to read and partially parse the input typed by the user.
 // Although your main class has to be in this file, there is no requirement that you
 // use this template or hav all or your classes in this file.
 
@@ -16,25 +16,26 @@ import java.net.Socket;
 //
 // This is an implementation of a simplified version of a command
 // line dictionary client. The only argument the program takes is
-// -d which turns on debugging output. 
+// -d which turns on debugging output.
 //
 
 
 public class CSdict {
     static final int MAX_LEN = 255;
     static Boolean debugOn = false;
-    
+
     private static final int PERMITTED_ARGUMENT_COUNT = 1;
     private static String command;
     private static String[] arguments;
     private static int SET_TIMEOUT_LIMIT = 30000;
-    private static Socket socket = null;
-    
+
+    private static Socket socket;
+
     public static void main(String [] args) {
-        byte cmdString[] = new byte[MAX_LEN];
-	int len;
-	// Verify command line arguments
-	
+        
+        int len;
+        // Verify command line arguments
+
         if (args.length == PERMITTED_ARGUMENT_COUNT) {
             debugOn = args[0].equals("-d");
             if (debugOn) {
@@ -48,70 +49,82 @@ public class CSdict {
             return;
         }
 
+        // Infinite while loop to keep program running
 
-	// Example code to read command line input and extract arguments.
-	
-        try {
-	    System.out.print("csdict> ");
-	    System.in.read(cmdString);
+        String userInput;
+        while (true) {
 
-	    // Convert the command string to ASII
-	    String inputString = new String(cmdString, "ASCII");
-	    
-	    // Split the string into words
-	    String[] inputs = inputString.trim().split("( |\t)+");
-	    // Set the command
-	    command = inputs[0].toLowerCase().trim();
-	    // Remainder of the inputs is the arguments. 
-	    arguments = Arrays.copyOfRange(inputs, 1, inputs.length);
 
-	    switch(command) {
-	    	case "open":
-	    		openCommand();
-	    		break;
-	    	case "dict":
-	    		dictCommand();
-	    		break;
-	    	case "set":
-	    		setCommand();
-	    		break;
-	    	case "define":
-	    		defineCommand();
-	    		break;
-	    	case "match":
-	    		matchCommand();
-	    		break;
-	    	case "prefixmatch":
-	    		prefixmatchCommand();
-	    		break;
-	    	case "close":
-	    		closeCommand();
-	    		break;
-	    	case "quit":
-	    		quitCommand();
-	    		break;
-	    	default:
-	    		System.err.println("998 Input error while reading commands, terminating.");
-	    }
+            // Example code to read command line input and extract arguments.
 
-	    // TODO: Delete this
-	    System.out.println("The command is: " + command);
-	    len = arguments.length;
-	    System.out.println("The arguments are: ");
-	    for (int i = 0; i < len; i++) {
-		System.out.println("    " + arguments[i]);
-	    }
-	    System.out.println("Done.");
-	    
-	    
-	} catch (IOException exception) {
-	System.err.println("998 Input error while reading commands, terminating.");
-            System.exit(-1);
-	}
+            try {
+                byte cmdString[] = new byte[MAX_LEN];
+                System.out.print("csdict> ");
+                System.in.read(cmdString);
+                // Convert the command string to ASII
+                String inputString = new String(cmdString, "ASCII");
+
+                // Split the string into words
+                String[] inputs = inputString.trim().split("( |\t)+");
+                // Set the command
+                command = inputs[0].toLowerCase().trim();
+                // Remainder of the inputs is the arguments.
+                arguments = Arrays.copyOfRange(inputs, 1, inputs.length);
+                switch(command) {
+                    case "open":
+                        String hostname = "dict.org";
+                        Integer portNumber = 2628;
+                        // if (arguments == null || arguments.length != 2) {
+                        //     System.err.println("901 Incorrect number of arguments.");
+                        //     return;
+                        // }
+                        // openSocketConnection(arguments[0], Integer.parseInt(arguments[1]));
+
+                        openSocketConnection(hostname, portNumber); // Hardcoded for easier testing
+                        break;
+                    case "dict":
+                        dictCommand();
+                        break;
+                    case "set":
+                        setCommand();
+                        break;
+                    case "define":
+                        defineCommand();
+                        break;
+                    case "match":
+                        matchCommand();
+                        break;
+                    case "prefixmatch":
+                        prefixmatchCommand();
+                        break;
+                    case "close":
+                        closeCommand();
+                        break;
+                    case "quit":
+                        quitCommand();
+                        break;
+                    default:
+                        System.err.println("900 Invalid command.");
+                }
+
+                // TODO: Delete this
+                // System.out.println("The command is: " + command);
+                // len = arguments.length;
+                // System.out.println("The arguments are: ");
+                // for (int i = 0; i < len; i++) {
+                //     System.out.println("    " + arguments[i]);
+                // }
+                // System.out.println("Done.");
+
+            } catch (IOException exception) {
+                System.err.println("998 Input error while reading commands, terminating.");
+                System.exit(-1);
+            }
+        }
     }
 
-/*  
- *	TODO: Opens a new TCP/IP connection to an dictionary server. 
+/*
+ *	TODO: Opens a new TCP/IP connection to an dictionary server.
  *	The server's name and the port number the server is listening
  *	on are specified by the command's parameters. The server name
  *	can be either a domain name or an IP address in dotted form.
@@ -119,82 +132,105 @@ public class CSdict {
  *	is considered an unexpected command if it is given when a control
  *	connection is already open.
 */
-private static void openCommand() {
-	String testHostName = "test.dict.org";
-	int testPort = 2628;
-	socket = openSocketConnection(testHostName, testPort);
-}
+
+// LITERALLY THIS IS NOT REQUIRED
+//private static void openCommand() {
+////    System.out.println("open command selected!");
+//	String hostname = "dict.org";
+//	Integer portNumber = 2628;
+//  // Socket socket = new Socket(hostName, portNumber);
+//	if (socket.isConnected()) {
+//		System.err.println("903 Supplied command not expected at this time.");
+//	} else {
+//		openSocketConnection(hostname, portNumber);
+//	}
+//}
 
 
-/*
- *	TODO: Retrieve and print the list of all the dictionaries
- *	the server supports. Each line will consist of a single
- *	word that is the the name of a dictionary followed by
- *	some information about the dictionary. You simply have
- *	to print each of these lines as returned by the server.
-*/
-private static void dictCommand() {
-	System.out.println("dictCommand() is called.");
-}
+    /*
+     *	TODO: Retrieve and print the list of all the dictionaries
+     *	the server supports. Each line will consist of a single
+     *	word that is the the name of a dictionary followed by
+     *	some information about the dictionary. You simply have
+     *	to print each of these lines as returned by the server.
+    */
+    private static void dictCommand() {
+        System.out.println("dictCommand() is called.");
+    }
 
-/*
- * TODO: Check the link man. I give up.
-*/
-private static void setCommand() {
-	System.out.println("setCommand() is called.");
-}
+    /*
+     * TODO: Check the link man. I give up.
+    */
+    private static void setCommand() {
+        System.out.println("setCommand() is called.");
+    }
 
-/*
- * TODO: Check the link man. I give up.
-*/
-private static void defineCommand() {
-	System.out.println("openCommand() is called.");
-}
+    /*
+     * TODO: Check the link man. I give up.
+    */
+    private static void defineCommand() {
+        System.out.println("openCommand() is called.");
+    }
 
-/*
- * TODO: Check the link man. I give up.
-*/
-private static void matchCommand() {
-	System.out.println("matchCommand() is called.");
-}
+    /*
+     * TODO: Check the link man. I give up.
+    */
+    private static void matchCommand() {
+        System.out.println("matchCommand() is called.");
+    }
 
-/*
- * TODO: Check the link man. I give up.
-*/
-private static void prefixmatchCommand() {
-	System.out.println("prefixmatchCommand() is called.");
-}
+    /*
+     * TODO: Check the link man. I give up.
+    */
+    private static void prefixmatchCommand() {
+        System.out.println("prefixmatchCommand() is called.");
+    }
 
-/*
- * TODO: Check the link man. I give up.
-*/
-private static void closeCommand() {
-	System.out.println("closeCommand() is called.");
-}
+    /*
+     * TODO: closeCommand closes the connection and is in a state waiting to open or quit
+    */
+    private static void closeCommand() {
+//	 try {
+//	 	if (socket.isClosed() || socket == null) {
+//	 		return;
+//	 	}
+//
+////		 out.close();
+////		 in.close();
+//		 socket.close();
+//	 } catch (IOException exception) {
+//	 	System.err.println("999 Processing error. You done fucked up.");
+//	 }
+    }
 
-/*
- * TODO: Check the link man. I give up.
-*/
-private static void quitCommand() {
-	System.out.println("quitCommand() is called.");
-}
+    /*
+     * TODO: What's the difference from closeCommand()
+     * need to do close before it can quit
+    */
+    private static void quitCommand() {
+        System.out.println("Good bye!");
+	    System.exit(0);
+    }
 
-private static String openSocketConnection(String hostName, int portNumber) {
-	try {
-		Socket echoSocket = new Socket(hostName, portNumber);
-	    PrintWriter out =
-	        new PrintWriter(echoSocket.getOutputStream(), true);
-	    BufferedReader in =
-	        new BufferedReader(
-	            new InputStreamReader(echoSocket.getInputStream()));
-	    String result = in.readLine();
-	    return result;
-	}
-	catch (IOException exception) {
-		System.err.println("998 Input error while reading commands, terminating.");
-		return exception.toString();
-	}
+    private static void openSocketConnection(String hostName, int portNumber) {
+        if (socket != null && socket.isConnected()) {
+            System.err.println("903 Supplied command not expected at this time. ");
+        }
+        try {
+            Socket socket = new Socket(hostName, portNumber);
+            PrintWriter out =
+                    new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in =
+                    new BufferedReader(
+                            new InputStreamReader(socket.getInputStream()));
+            BufferedReader stdIn =
+                    new BufferedReader(
+                            new InputStreamReader(System.in));
+            System.out.println(in.readLine());
+        }
+        catch (IOException exception) {
+            System.err.println("998 Input error while reading commands, terminating.");
+            System.exit(-1);
+        }
+    }
 }
-}
-    
-    
